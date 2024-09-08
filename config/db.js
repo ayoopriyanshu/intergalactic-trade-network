156914c7b2cb52450f354dca282c9f5409474121
ayoopriyanshu <112677512+ayoopriyanshu@users.noreply.github.com>
2024-09-08 20:47:40 +0530

    Integrate mongodb cluster

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Connected to mongodb");
+    } catch (error) {
+        console.log('Error connecting to mongodb: ', error.message);
+        process.exit(1);
+    }
+}
+
+export default connectDB
